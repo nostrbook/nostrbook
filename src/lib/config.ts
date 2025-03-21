091c4ca5f5c973e89047abf0b1ec5541ae34cf70
asmcos <asmcos@blx.com>
2025-03-21 16:09:32 +0800

    add config

---
+++ src/lib/config.ts
@@ -0,0 +1,20 @@
+export const defaultUploaderURLs = [
+  
+
+  'https://nostrcheck.me',
+  'https://relay.shawnyeager.com/',
+  'https://nostr.build',
+  'https://yabu.me',
+  'https://nostpic.com',
+  'https://void.cat',
+  'https://files.sovbit.host',
+  'https://blossom.nogood.studio',
+
+];
+
+export const defaultRelays = [
+ 
+  'wss://relay.j35tr.com', 
+  'wss://relay.damus.io/',
+  'wss://relay.primal.net/',
+];
