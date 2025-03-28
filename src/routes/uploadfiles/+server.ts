aa9bbd261486dd7628931b447fc247086bec7a9c
asmcos <asmcos@blx.com>
2025-03-28 13:16:18 +0800

    删除host

--- src/routes/uploadfiles/+server.ts
+++ src/routes/uploadfiles/+server.ts
@@ -26,7 +26,7 @@ export const GET: RequestHandler = async ({ request }) => {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Host': 'nostr.com'
+                 
             },
             responseType: 'arraybuffer'
         });
