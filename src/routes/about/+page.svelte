f6deb2e3c111728b5390136d0417a4b7c050142b
asmcos <asmcos@blx.com>
2025-04-14 09:50:57 +0800

    增加了github链接

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
      
 </script>
 
-    <div class="container mx-auto p-8 space-y-6">
+    <div class="container mx-auto p-8 space-y-6 bg-white rounded-lg shadow-md max-w-3xl">
         <h1 class="text-3xl font-bold text-center text-gray-800">nostrbook 开启知识传承新篇</h1>
         <p class="text-lg text-gray-700">
             “博学而笃志，切问而近思。” 每一次指尖于键盘上的轻快跳跃，皆为知识薪火的郑重传递。我们如同古代贤能一般，在此汇聚各方智慧，慷慨分享所学所悟。
@@ -10,4 +10,12 @@
         <p class="text-lg text-gray-700">
             恰似古人在竹简、宣纸之上著书立说，将他们的思想火种播撒四方。我们亦在这数字世界中，让知识汇聚成滔滔江河，奔腾不息，冲破时间与空间的藩篱，去滋养一代又一代求知若渴者的心田。
         </p>
+        <div class="flex justify-center">
+            <a href="https://github.com/nostrbook/nostrbook"
+                class="flex items-center space-x-2 text-blue-600 hover:text-blue-800 transition-colors duration-300">
+                <i class="fa-solid fa-cat"></i>
+                <span>查看 GitHub 仓库</span>
+            </a>
+        </div>
     </div>
+
