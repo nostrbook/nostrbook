1082383ab6a589323342df55b88a8d7611c2dd11
asmcos <asmcos@blx.com>
2025-04-24 10:12:06 +0800

    add raw page 内容这样 server.ts和页面浏览都有内容

---
+++ src/routes/blog/[blogId]/[mdfile]/+page.svelte
@@ -0,0 +1,18 @@
+
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  
+  let data = '';
+  
+  onMount(async () => {
+    const response = await fetch(''); // 对应 +server.ts 的路由
+    data = await response.text();
+  });
+</script>
+
+<pre>
+{data}
+</pre>
+
+
